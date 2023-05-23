47a17d22995a2914a7b0bcaa7888d87a56dba066
jmnicolas4me@gmail.com <jmnicolas4me@gmail.com>
2023-05-23 10:52:50 +0800

    update

--- app/Http/Controllers/InnOrderDetailsController.php
+++ app/Http/Controllers/InnOrderDetailsController.php
@@ -29,19 +29,20 @@ class InnOrderDetailsController extends Controller
         $inn = Inn::where('user_id', Auth::user()->id)->get();
         $rooms = Room::select('*')->where('inn_id', $inn[0]->id)->get();
         $freebies = Freebie::all();
-        $transactions = Transaction::all();
         $reservations = Reservation::all();
         $order_details = OrderDetail::where('inn_id', $inn[0]->id)->get();
-
+        $transactions = Transaction::where('inn_id', $inn[0]->id)->get();
+    
         return view('admin.inns.show.order_details.index')
-        ->with('inn', $inn)
-        ->with('freebies', $freebies)
-        ->with('transactions', $transactions)
-        ->with('reservations', $reservations)
-        ->with('order_details', $order_details)
-        ->with('id', $inn[0]->id)
-        ->with('rooms', $rooms);
+            ->with('inn', $inn)
+            ->with('freebies', $freebies)
+            ->with('transactions', $transactions)
+            ->with('reservations', $reservations)
+            ->with('order_details', $order_details)
+            ->with('id', $inn[0]->id)
+            ->with('rooms', $rooms);
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -64,25 +65,32 @@ class InnOrderDetailsController extends Controller
         $product = Product::find($request->product_id);
         $product->quantity = $product->quantity - $request->quantity;
         $product->save();
-        OrderDetail::create([
-            'order_number' => $request->order_number,
-            'quantity' => $request->quantity,
-            'price' => $product->price,
-            'subtotal' => $request->quantity * $product->price,
-            'product_id' => $request->product_id, 
-            'inn_id' => $request->id,
-            'is_deleted' => 0
-        ]);
-        $order_summary = OrderSummary::where('inn_id', $request->id)->where('is_deleted', 0)->get();
-        $products = Product::where('inn_id', $request->id)->where('is_deleted', 0)->get();
-        $order_details = OrderDetail::where('inn_id', $request->id)->where('is_deleted', 0)->get();
-        $rooms = Room::where('inn_id', $request->id)->get();
-        return redirect()->back()
+
+OrderDetail::create([
+    'order_number' => $request->order_number,
+    'quantity' => $request->quantity,
+    'price' => $product->price,
+    'subtotal' => $request->quantity * $product->price,
+    'product_id' => $request->product_id, 
+    'inn_id' => $request->id,
+    'is_deleted' => 0
+]);
+
+    $order_summary = OrderSummary::where('inn_id', $request->id)->where('is_deleted', 0)->get();
+    $products = Product::where('inn_id', $request->id)->where('is_deleted', 0)->get();
+    $order_details = OrderDetail::where('inn_id', $request->id)->where('is_deleted', 0)->get();
+    $rooms = Room::where('inn_id', $request->id)->get();
+    $transactions = Transaction::where('inn_id', $request->inn_id)->get();
+
+    return redirect()->back()
         ->with('products', $products)
         ->with('rooms', $rooms)
         ->with('order_details', $order_details)
+        ->with('transactions', $transactions) // Pass the $transactions variable to the view
         ->with('last_id', count($order_summary) > 0 ? $order_summary->last()->id : 1)
         ->with('id', $request->id);
+
+        
     }
 
     /**
--- resources/views/user/dashboard.blade copy.php
+++ resources/views/user/dashboard.blade copy.php
@@ -47,7 +47,48 @@
                                 <button type="submit" class="btn btn-primary">Save</button>
                             </form>
                         </div>
-                        
+                        <div class="bg-secondary mt-4 rounded d-flex align-items-center justify-content-between p-4">
+                            <form action="{{ route('order-details.store') }}" class="w-100" method="post">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $id }}">
+                                <input type="hidden" name="order_number" value="vcw-{{$id}}-ams-{{$last_id}}">
+
+                                <div>
+                                    <div class="mb-3">
+                                        <select name="product_id" class="form-select mb-3"
+                                            aria-label="Default select example" required>
+                                            <option value="">Select Transaction</option>
+                                            @if (!is_null($transactions))
+                                                @foreach ($products as $product)
+                                                    <option value="{{ $product->id }}">{{ $product->name }} - {{$product->category_id}}
+                                                    </option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                    </div>
+                                    {{-- <div class="mb-3">
+                                        <select name="room_id" class="form-select mb-3"
+                                            aria-label="Default select example" required>
+                                            <option value="">Select Room #</option>
+                                            @if (!is_null($transactions))
+                                                @foreach ($rooms as $room)
+                                                    <option value="{{ $room->id }}">Room #{{ $room->room_number }}
+                                                    </option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                    </div> --}}
+                                    <input type="hidden" name="inn_id" value="{{ $id }}">
+                                    <div class="mb-3">
+                                        <label for="" class="mb-2">Quantity: </label>
+                                        <input type="number" required name="quantity"
+                                            class="form-control mb-3" />
+                                    </div>
+                                   
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
                     </div>
                     <div class="col-sm-6 col-xl-6">
                         <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
