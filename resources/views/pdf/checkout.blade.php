236c229fa1cfa3e79da65d41ac12eb2c49c6a9b8
jmnicolas4me@gmail.com <jmnicolas4me@gmail.com>
2023-05-25 12:16:49 +0800

    update

--- resources/views/pdf/checkout.blade.php
+++ resources/views/pdf/checkout.blade.php
@@ -1,6 +1,8 @@
+
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         /* Add any custom styles for the PDF */
     </style>
@@ -37,5 +39,7 @@
     <p>Payment: Php {{ $paymentInput }}</p>
 
     <p>Change: Php {{ $change }}</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
